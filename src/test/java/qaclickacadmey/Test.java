6f83c7a22e599f57685e1de03102915e502d19ce
bush <bushmakol@gmail.com>
2022-09-28 08:49:05 +0200

    jdk

--- src/test/java/qaclickacadmey/Test.java
+++ src/test/java/qaclickacadmey/Test.java
@@ -4,8 +4,6 @@ public class Test {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello1");
-		System.out.println("Hello 2");
 		System.out.println("Heloo 3 by Git Demo");
 		System.out.println("Added by gitx branch1");
 
