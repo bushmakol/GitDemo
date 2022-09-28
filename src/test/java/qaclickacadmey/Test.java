8034b4841f39307bf92b884d5df7bfeb4f6b3eea
bush <bushmakol@gmail.com>
2022-09-28 08:27:55 +0200

    gitxA commit in branch1

--- src/test/java/qaclickacadmey/Test.java
+++ src/test/java/qaclickacadmey/Test.java
@@ -7,6 +7,7 @@ public class Test {
 		System.out.println("Hello1");
 		System.out.println("Hello 2");
 		System.out.println("Heloo 3 by Git Demo");
+		System.out.println("Added by gitx branch1");
 
 	}  
 
